fa6e2703b64030702fd3a5a3bd7ad4e49216933c
github username <github email address>
2022-02-01 16:17:37 -0500

    initial commit

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const login: FC = () => {
+    return (
+        <div>
+            <h1>This is login page</h1>
+        </div>
+    )
+}
+
+export default login;
